3013341769cc3f6f8622c6847ad7c455f034907f
David Korczynski <david@adalogics.com>
2024-12-02 22:21:03 +0000

    2024-10-26
    Signed-off-by: David Korczynski <david@adalogics.com>

--- create-zip.sh
+++ create-zip.sh
@@ -2,4 +2,5 @@ DST_FOLDER=$1
 
 cp $1/db-timestamps.json .
 cp $1/all-project-timestamps.json .
-zip -jr db-stamp.zip all-project-timestamps.json db-timestamps.json
+#zip -jr db-stamp.zip all-project-timestamps.json db-timestamps.json
+tar cfJ db-stamp.tar.xz all-project-timestamps.json db-timestamps.json
